4fc2116b16e677b24ca3db89b2054ab8a64ec202
Cpt_Crunch <jasong84@gmail.com>
2024-07-06 20:05:36 -0500

    Modify inngest exports

--- src/routes/api/inngest/+server.ts
+++ src/routes/api/inngest/+server.ts
@@ -2,8 +2,7 @@ import { functions, inngest } from '$lib/inngest';
 import INNGEST_SIGNING_KEY from '$env/static/private';
 import { serve } from 'inngest/sveltekit';
 
-export const { GET, POST, PUT } = serve({ 
-  client: inngest, 
-  functions,
-  signingKey: INNGEST_SIGNING_KEY
-});
+const inngestServe = serve({ client: inngest, functions, signingKey: `${INNGEST_SIGNING_KEY}`})
+export const GET = inngestServe.GET;
+export const POST = inngestServe.POST;
+export const PUT = inngestServe.PUT;
