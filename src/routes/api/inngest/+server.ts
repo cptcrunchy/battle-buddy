2df1107ecb036da237a2e72f27bca64d14bde13c
Cpt_Crunch <jasong84@gmail.com>
2024-07-06 20:45:58 -0500

    Fix inngest key import

--- src/routes/api/inngest/+server.ts
+++ src/routes/api/inngest/+server.ts
@@ -1,5 +1,5 @@
 import { functions, inngest } from '$lib/inngest';
-import INNGEST_SIGNING_KEY from '$env/static/private';
+import { INNGEST_SIGNING_KEY } from '$env/static/private';
 import { serve } from 'inngest/sveltekit';
 
 const inngestServe = serve({ client: inngest, functions, signingKey: `${INNGEST_SIGNING_KEY}`})
